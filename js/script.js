764869d1a06da523e5e971adec1d342ac06a5eb0
unknown <zlatanfirmansyah0@gmail.com>
2024-08-20 15:11:29 +0700

    coba1

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika menu di klik
+document.querySelector("#menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
